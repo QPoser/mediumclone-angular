cfbc24f47af98050188503f20f69896ab416c1df
andrey.erdikov <erdikov.andrey@skyeng.ru>
2021-11-21 01:24:52 +0700

    Added template and module

---
+++ src/app/auth/components/register/register.component.ts
@@ -0,0 +1,30 @@
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validator, Validators} from '@angular/forms';
+
+@Component({
+  selector: 'mc-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.scss'],
+})
+export class RegisterComponent implements OnInit {
+  form: FormGroup;
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.initializeForm();
+  }
+
+  private initializeForm(): void {
+    this.form = this.fb.group({
+      username: ['', Validators.required],
+      email: '',
+      password: '',
+    });
+  }
+
+  onSubmit(): void {
+    console.log(this.form.valid);
+    console.log(this.form.value);
+  }
+}
